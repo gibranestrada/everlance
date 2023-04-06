735bbc1de302761af91449b87d330d04285ecb2e
Gibran <gibran9@gmail.com>
2023-04-06 15:27:35 -0400

    Created html and css for search input and added search icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import SearchIcon from "./assets/search-icon.svg";
 
 function App() {
   return (
@@ -6,6 +7,26 @@ function App() {
       <header className="App-nav drop-shadow-md border-b-4">
         <nav className="p-6 pl-7 font-semibold">Everlance Inc.</nav>
       </header>
+      <div className="grid justify-center p-3 pt-12">
+        <section className="w-128 flex justify-between">
+          <p className="font-semibold text-xl">GitHub Users</p>
+          <div className="flex flex-row">
+            <img
+              className="search-icon"
+              src={SearchIcon}
+              width="18"
+              alt="Search icon"
+            />
+            <input
+              className="border-2 rounded p-1 pl-7 w-72"
+              type="search"
+              placeholder="Enter username or email"
+            ></input>
+          </div>
+        </section>
+        <section className="w-128">world</section>
+        <section className="w-128">!</section>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: { width: { 128: "48rem" } },
   },
   plugins: [],
 };
